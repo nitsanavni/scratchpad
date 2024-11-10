0e6f4cde4d9e6d6e18f8bbdd06b65a44eeeefb8b
Nitsan Avni <nitsanav@gmail.com>
2024-11-10 09:08:06 +0100

    - F fzf --tmux

--- vim-to-fzf-and-back/fzf_output_the_filtered_down_list.sh
+++ vim-to-fzf-and-back/fzf_output_the_filtered_down_list.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# Original command to run fzf
-# selected=$(fzf)
-
-# Update the command to use the '--multi' option for multi-selection
-selected=$(fzf --multi --bind 'ctrl-a:select-all')
-
-# Output all selected items, or, if none are selected, all filtered items
-echo "$selected"
+fzf --tmux --multi --bind 'ctrl-a:select-all'
