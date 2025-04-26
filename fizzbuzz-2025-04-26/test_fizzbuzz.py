9ea593392d65caaab39e16b4754dafdf41e15d53
Nitsan Avni <nitsanav@gmail.com>
2025-04-26 09:55:17 +0200

    Fix assertion for number 30 in FizzBuzz test

--- fizzbuzz-2025-04-26/test_fizzbuzz.py
+++ fizzbuzz-2025-04-26/test_fizzbuzz.py
@@ -19,7 +19,7 @@ def test_fizzbuzz():
     assert output_lines[2] == "Fizz", "3 should be Fizz"
     assert output_lines[4] == "Buzz", "5 should be Buzz"
     assert output_lines[14] == "FizzBuzz", "15 should be FizzBuzz"
-    # Intentionally incorrect assertion - 30 is FizzBuzz but we're saying it should be Fizz
-    assert output_lines[29] == "Fizz", "30 should be FizzBuzz"
+    # 30 is divisible by both 3 and 5, so it should be FizzBuzz
+    assert output_lines[29] == "FizzBuzz", "30 should be FizzBuzz"
     assert output_lines[98] == "Fizz", "99 should be Fizz"
     assert output_lines[99] == "Buzz", "100 should be Buzz"
